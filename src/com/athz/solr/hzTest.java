82ac3f4f53059b43bea163e0dfd89f9dc3604a1f
pengsaisai <441223577@qq.com>
2017-09-06 22:14:33 +0800

    ssss

--- src/com/athz/solr/hzTest.java
+++ src/com/athz/solr/hzTest.java
@@ -3,5 +3,9 @@ package com.athz.solr;
 public class hzTest {
     public static void main(String[] args) {
 		System.out.println("ssss");
-	}
+		System.out.println("ssss");
+		System.out.println("ssss");
+		System.out.println("ssss");
+		System.out.println("ssss");
+	} 
 }
